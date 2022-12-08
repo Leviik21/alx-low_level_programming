282a9ea43a9465a48342414e639b4c94545d36c1
Levi ikechukwu <Leviikechukwu12@gmail.com>
2022-12-08 11:16:32 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
-
 /**
- * main  -  Entry Point
- * 
+ * main  -  Entry Point 
  * Return:  Always 0 (success)
  */
+
  int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return (0);
+	return (0) ;
 
 
 }
