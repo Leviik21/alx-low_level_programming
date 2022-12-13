edead14efd48db2752ef9ebb7eedf17fcb312575
Levi ikechukwu <Leviikechukwu12@gmail.com>
2022-12-13 06:13:38 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+ * main - prints _putchar to the screen
+ *
+ * Retrun: Always 0
+ */
+int main(void)
+{
+	int i;
+	char c[] = "_putchar";
+
+	for (i = 0, i <= 7, i++)
+	{
+		_putchar(c[i]);
+
+	}
+
+	_putchar('/n');
+
+	return (0);
+}
