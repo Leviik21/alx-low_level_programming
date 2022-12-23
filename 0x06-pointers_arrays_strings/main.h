ffbb5f7e630e0956497bbe6aa233d681aef7d92c
Levi ikechukwu <Leviikechukwu12@gmail.com>
2022-12-22 20:40:59 -0800

    Task 101

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,7 +7,6 @@
  *char prototypes()
  */
 void reverse_array(int *a, int n);
-void print_number(int n);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
@@ -19,5 +18,5 @@ char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
-
+void print_number(int n);
 #endif /*MAIN_H*/
